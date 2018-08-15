1c225e078e6eba89e928822792112afa3c507ffb
sgolic <s.golic@levi9.com>
2018-08-15 13:23:31 +0200

    Added book service

---
+++ demo/frontend/react-library/src/app/services/book.service.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { HTTP_HEADERS } from "../auth";
+
+const API = "http://localhost:8080/categories";
+
+export default class BookService {
+  getBooks() {
+    return axios.get(API, { headers: HTTP_HEADERS }).then(result => {
+      return result.data;
+    });
+  }
+}
